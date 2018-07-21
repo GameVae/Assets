3af571858d081bc80e3a1b7687c3a04ae83f1d7a
HoManhQuan112 <homanhquan87@gmail.com>
2018-07-21 17:31:47 +0700

    end

---
+++ Scripts/LoginScene/LoadingUI/LoadingUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingUI : MonoBehaviour {
+    public static LoadingUI instance;
+    public Slider LoadingSlider;
+    private Text loadingTxt;
+
+    private void Awake()
+    {
+        loadingTxt = LoadingSlider.GetComponentInChildren<Text>();
+        instance = this;
+    }
+    void Start () {
+		
+	}
+    public void SetLoadingText(string content)
+    {
+        loadingTxt.text = content;
+    }
+    public void SetSliderValue (int value)
+    {
+        LoadingSlider.value = value;
+    }
+}
