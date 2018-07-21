ac400fd165374f1956ea457ae154cacdea245b7c
HoManhQuan112 <homanhquan87@gmail.com>
2018-07-21 12:01:29 +0700

    7/21/2018 12:01

---
+++ Scripts/testScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript: MonoBehaviour
+{
+    public A thisA;
+}
+
+[SerializeField]
+public class A {
+    public int mong;
+    public Dictionary<string, B> listB;
+}
+[SerializeField]
+public class B 
+{
+    public int vi;
+}
