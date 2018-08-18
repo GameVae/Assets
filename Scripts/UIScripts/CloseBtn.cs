404426f178b065d207b55cbfb3523b2d282c7b01
HoManhQuan112 <homanhquan87@gmail.com>
2018-08-18 17:49:13 +0700

    update 1808

---
+++ Scripts/UIScripts/CloseBtn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CloseBtn : MonoBehaviour {	
+	void Awake () {
+        GameObject parentGO = transform.parent.gameObject;
+        Button closeBtn = gameObject.GetComponent<Button>();
+        closeBtn.onClick.AddListener(() =>
+        {
+            parentGO.SetActive(false);
+        });
+	}
+
+}
