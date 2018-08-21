f8a3ddc091b6204daa9762e503c315ecbea89320
HoManhQuan112 <homanhquan87@gmail.com>
2018-08-21 07:49:03 +0700

    test

---
+++ Scripts/Effect/testEffect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testEffect : MonoBehaviour {
+    private ParticleSystem particleSystem;
+
+    private void Awake()
+    {
+        particleSystem = transform.GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        
+    }
+}
