a247a4479331ea43623012725836b385cee4c8f3
HoManhQuan112 <homanhquan87@gmail.com>
2018-08-30 07:56:24 +0700

    30

---
+++ Scripts/Test/Rotator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour
+{
+    public float Speed;
+}
+class RotatorSystem : ComponentSystem
+{
+    struct ComponentsEntiti
+    {
+        public Rotator Rotator;
+        public Transform Transform;
+    }
+    float deltaTime;
+    protected override void OnStartRunning()
+    {
+        
+        base.OnStartRunning();
+        deltaTime = Time.deltaTime;
+    }
+
+    protected override void OnUpdate()
+    {
+        
+        foreach (var e in GetEntities<ComponentsEntiti>())
+        {
+            e.Transform.Rotate(0, e.Rotator.Speed * deltaTime, 0f);
+          }
+        //throw new System.NotImplementedException();
+    }
+}
