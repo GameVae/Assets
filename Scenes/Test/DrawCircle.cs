26edb16299e8117f684fc1142bf31d64c7540d99
Nguyễn Đinh Toàn <ndinhtoan48@gmail.com>
2018-10-19 13:40:55 +0700

    Progress operation effect

---
+++ Scenes/Test/DrawCircle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    public LineRenderer lineRender;
+
+    private void Start()
+    {
+        int radiusPerSegment = 10;
+        int count = 360 / radiusPerSegment;
+        lineRender.positionCount = count + 1;
+
+        for(int i = 0; i < count + 1; i++)
+        {
+            float x = Mathf.Cos((radiusPerSegment * i) * Mathf.Deg2Rad) * 3;
+            float y = Mathf.Sin((radiusPerSegment * i) * Mathf.Deg2Rad) * 3;
+            lineRender.SetPosition(i, new Vector3(x, 0.5f, y));
+        }
+    }
+}
---
+++ Scripts/Test/ProgressController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressController : MonoBehaviour
+{
+    private bool isActive;
+    private Image image;
+    private Color defaultColor;
+
+    public float Angular;
+    public Graphic Background;
+    private void Awake()
+    {
+        image = GetComponentInChildren<Image>();
+
+        isActive = false;
+        image.enabled = isActive;
+        if(Background != null)
+        {
+            defaultColor = Background.color;
+        }
+    }
+
+    public void Active(bool value)
+    {
+        isActive = value;
+        image.enabled = isActive;
+        if (Background != null)
+        {
+            if (value)
+            {
+                Color temp = defaultColor;
+                temp *= 0.75f;
+                Background.color = temp;
+            }
+            else
+            {
+                Background.color = defaultColor;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isActive)
+        {
+            image.transform.Rotate(0, 0, Angular * Time.deltaTime);
+        }
+        if(!isActive)
+        {
+            if(Application.internetReachability == NetworkReachability.NotReachable)
+            {
+                Active(true);
+            }
+        } else
+        {
+            if (Application.internetReachability != NetworkReachability.NotReachable)
+            {
+                Active(false);
+            }
+        }
+    }
+}
