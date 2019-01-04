e9bbeb0533473a2a38deaab9556c1f09125a2e54
HoManhQuan112 <homanhquan87@gmail.com>
2019-01-04 09:58:43 +0700

    fix ui login

--- Scripts/Test/Data/PlayerInformation.cs
+++ Scripts/Test/Data/PlayerInformation.cs
@@ -2,19 +2,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class PlayerInformation : MonoBehaviour {
-    public static PlayerInformation instance;
+[Serializable]
+public class Base_User_Info
+{
+
+}
+[Serializable]
+public class Rss_User_Info
+{
+    public Vector3Int Position_Rss_Cell;
+    public int Quality;
+}
+[Serializable]
+public class User_Info
+{
     public int ID_User;
     public int ID_Server;
-    [Space]
-    public Connection Connection;
+    public int Diamond;  
+    public int? Guild_ID;
+    public int? Last_Guild_ID;
+    public int Might;
+    public int Killed;
 
-    public PlayerInformation(int iD_User, int iD_Server)
+    public string NameInGame;
+    public string ChatWorldColor;
+    public string GuildName;
+
+    public User_Info(int iD_User, int iD_Server, int diamond, int? guild_ID, int? last_Guild_ID, int might, int killed, string nameInGame, string chatWorldColor, string guildName)
     {
         ID_User = iD_User;
         ID_Server = iD_Server;
+        Diamond = diamond;
+        Guild_ID = guild_ID;
+        Last_Guild_ID = last_Guild_ID;
+        Might = might;
+        Killed = killed;
+        NameInGame = nameInGame;
+        ChatWorldColor = chatWorldColor;
+        GuildName = guildName;
     }
+}
+public class PlayerInformation : MonoBehaviour {
+    public static PlayerInformation instance;
+    [Space]
+    public User_Info User_Info;
+    [Space]
+    public Base_User_Info[] Base_User_Info = new Base_User_Info[5];
+    [Space]
+    public Rss_User_Info[] rss = new Rss_User_Info[10];
+    [Space]
+    private Connection Connection;
+
+    
 
     void Awake () {
         if (instance == null) { instance = this; }
@@ -29,7 +70,7 @@ public class PlayerInformation : MonoBehaviour {
     {
         int ID_User = int.Parse((obj.data.GetField("ID_User").ToString()));
         int ID_Server = int.Parse((obj.data.GetField("ID_Server").ToString()));
-        PlayerInformation playerInformation = new PlayerInformation(ID_User, ID_Server);
+        Debug.Log("gán vào Scriptable Object");
     }
 }
 
