be564fbd0c4010139446f6113e9f19b1c031dea4
ndinhtoan47 <ndinhtoan48@gmail.com>
2019-08-17 16:27:10 +0700

    update

--- Scripts/UIScripts/Windows/Guild/FindGuildFunc.cs
+++ Scripts/UIScripts/Windows/Guild/FindGuildFunc.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using SocketIO;
+using System.Collections.Generic;
 using UI.Composites;
 using UnityEngine;
 
@@ -16,7 +17,10 @@ public class FindGuildFunc : ToggleWindow
     private void Awake()
     {
         FindBtn.OnClickEvents += OnFindGuidlButton;
+
         GuildTag.AcceptBtn.OnClickEvents += S_APPLY_GUILD;
+
+        GuildSys.EventController.On("R_SEARCH_GUILD", R_SEARCH_GUILD);
     }
 
     public override void Load(params object[] input)
@@ -32,10 +36,10 @@ public class FindGuildFunc : ToggleWindow
 
     protected void OnFindGuidlButton()
     {
-
+        S_SEARCH_GUILD();
     }
 
-    protected void S_APPLY_GUILD()
+    private void S_APPLY_GUILD()
     {
         Dictionary<string, string> applyInfo = new Dictionary<string, string>()
         {
@@ -44,5 +48,47 @@ public class FindGuildFunc : ToggleWindow
         JSONObject data = new JSONObject(applyInfo);
 
         GuildSys.EventController.Emit("S_APPLY_GUILD", data);
+    }
+
+    private void S_SEARCH_GUILD()
+    {
+        string gName = string.IsNullOrEmpty(GuildNameInput.Text) ? null : GuildNameInput.Text;
+        string gTag = string.IsNullOrEmpty(GuildTagInput.Text) ? null : GuildTagInput.Text;
+
+        if(gName != null || gTag != null)
+        {
+            gName = gName ?? "null";
+            gTag = gTag ?? "null";
+
+            Dictionary<string, string> searchInfo = new Dictionary<string, string>()
+            {
+                {"GuildTag" , gTag },
+                {"GuildName", gName}
+            };
+
+            JSONObject data = new JSONObject(searchInfo);
+            GuildSys.EventController.Emit("S_SEARCH_GUILD", data);
+        }
+    }
+
+    private void S_GET_GUILD_INFO()
+    {
+        Dictionary<string, string> searchInfo = new Dictionary<string, string>()
+            {
+                {"Guild_ID" , "" },                
+            };
+
+        JSONObject data = new JSONObject(searchInfo);
+        GuildSys.EventController.Emit("S_GET_GUILD_INFO", data);
+    }
+
+    private void R_GET_GUILD_INFO(SocketIOEvent obj)
+    {
+        Debugger.Log(obj);
+    }
+
+    private void R_SEARCH_GUILD(SocketIOEvent obj)
+    {
+        Debugger.Log(obj);
     }
 }
